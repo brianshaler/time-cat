a14afda7d3ff4e1655570373306a50f8ffb94823
Brian Shaler <brian@shaler.name>
2014-07-07 21:15:07 -0700

    add leaderboard

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var start = process.hrtime();
 // Hurry up and start the timer!
 
 var colors = require('colors');
+var leaderboard = require('./leaderboard');
 var party = require('./party');
+var userName = process.argv[2];
 
 function displayTime (time) {
   var str = time + 's';
@@ -21,7 +23,14 @@ process.on('SIGINT', function () {
   var hrtime = process.hrtime(start);
   var time = hrtime[0] + hrtime[1] / 1000000000;
   displayTime(time);
-  process.exit();
+  if (userName && userName.length > 0) {
+    leaderboard.save(userName, time, Date.now(), function (err) {
+      if (err) { throw err; }
+      process.exit();
+    });
+  } else {
+    process.exit();
+  }
 });
 
 var messages = [
@@ -55,3 +64,38 @@ messages.forEach(function (message) {
     }, d);
   })(message, delay);
 });
+
+if (userName && userName == 'leaderboard') {
+  leaderboard.get(function (err, entries) {
+    if (err) { throw err; }
+    if (process.argv[3] != 'all') {
+      entries = entries.filter(function (entry) {
+        return entry.score < 1;
+      });
+    }
+    entries.sort(function (a, b) {
+      return Math.abs(a.score-1) < Math.abs(b.score-1) ? -1 : 1;
+    });
+    var maxEntries = 7;
+    if (entries.length > maxEntries) {
+      entries.splice(maxEntries, entries.length - maxEntries);
+    }
+    var longestName = 1;
+    entries.forEach(function (entry) {
+      longestName = longestName > entry.name.length ? longestName : entry.name.length;
+    });
+    
+    console.log('\nTop scores:');
+    console.log('============================');
+    entries.forEach(function (entry) {
+      var name = entry.name;
+      var whitespace = '';
+      while (name.length + whitespace.length < longestName) {
+        whitespace += ' ';
+      }
+      console.log(name + ':  ' + whitespace + entry.score);
+    });
+    console.log('============================\n');
+    process.exit();
+  });
+}
---
+++ leaderboard.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+
+var leaderboard = './leaderboard.txt';
+
+module.exports = {
+  save: function (name, score, time, next) {
+    var line = name + ',' + score + ',' + time + '\n';
+    fs.appendFile('./leaderboard.txt', line, next);
+  },
+  get: function (next) {
+    var file = fs.createReadStream(leaderboard, {encoding: 'utf8'});
+    var data = '';
+    file.on('data', function (chunk) {
+      data += chunk;
+    });
+    file.on('end', function () {
+      var lines = data.split('\n');
+      var entries = [];
+      lines.forEach(function (line) {
+        var fields = line.split(',');
+        if (fields.length == 3) {
+          entries.push({
+            name: fields[0],
+            score: fields[1],
+            time: fields[2]
+          });
+        }
+      });
+      next(null, entries);
+    });
+  }
+};
